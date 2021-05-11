f5e6ec64c124209b9bb083b5c4147f8271dff08a
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-05-11 09:51:23 +0200

    test

--- app/Http/Controllers/AllegroController.php
+++ app/Http/Controllers/AllegroController.php
@@ -109,16 +109,16 @@ class AllegroController extends Controller
 
     
 
-    public static function refreshTokenRepo()
+    public function refreshTokenRepo()
     {
         $clientId = '1842f4e03d1347d4812246f7439baaa1';
         $clientSecret = 'JvRLfxOdGmLBNRooPqnxQJOKFnwZ7XW1bW5m7tPCNb1LPaw5ttje2g7Fcz0OS6ri';
         $token = UserData::where('user_id', 7)->get()[1];
         // $refresh_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiI5Mzk3OTA3NiIsInNjb3BlIjpbImFsbGVncm86YXBpOm9yZGVyczpyZWFkIiwiYWxsZWdybzphcGk6cHJvZmlsZTp3cml0ZSIsImFsbGVncm86YXBpOnNhbGU6b2ZmZXJzOndyaXRlIiwiYWxsZWdybzphcGk6YmlsbGluZzpyZWFkIiwiYWxsZWdybzphcGk6Y2FtcGFpZ25zIiwiYWxsZWdybzphcGk6ZGlzcHV0ZXMiLCJhbGxlZ3JvOmFwaTpiaWRzIiwiYWxsZWdybzphcGk6c2FsZTpvZmZlcnM6cmVhZCIsImFsbGVncm86YXBpOm9yZGVyczp3cml0ZSIsImFsbGVncm86YXBpOmFkcyIsImFsbGVncm86YXBpOnBheW1lbnRzOndyaXRlIiwiYWxsZWdybzphcGk6c2FsZTpzZXR0aW5nczp3cml0ZSIsImFsbGVncm86YXBpOnByb2ZpbGU6cmVhZCIsImFsbGVncm86YXBpOnJhdGluZ3MiLCJhbGxlZ3JvOmFwaTpzYWxlOnNldHRpbmdzOnJlYWQiLCJhbGxlZ3JvOmFwaTpwYXltZW50czpyZWFkIl0sImFsbGVncm9fYXBpIjp0cnVlLCJhdGkiOiIxMjUyNjIxNy1iZDI4LTQ2MTAtOTg5YS0xZWRhODZjMDc2NzMiLCJleHAiOjE2MjU1NjIwOTQsImp0aSI6IjVjN2ViNzE3LTQ3YmUtNDAwYS04ZjhiLTM3YzZiMjU4MmY2YyIsImNsaWVudF9pZCI6IjE4NDJmNGUwM2QxMzQ3ZDQ4MTIyNDZmNzQzOWJhYWExIn0.TJah9nT8ZXLDRoUGyovgUM9suoldaNDeJAqjMfa6yZOzWrHPRfWaTluhTC0---XUh48_B6YC7mpXerqEnwcBoNa__LiastBu5rhzI3Jw_UwQF_BFYH7VD4QLIBrc2YWYe4wdyuIcRkq4wg2LumThAvJgsma0kAs518TJ5UCbsFpUYc3UcnNtrIdMgtVRAY0xxe-slMzDWhFIZ5It_JcAm_aRCZ31NlLXdFnx9UpP4ZgbKnYHo19kT0fGmNw4RDucVhKRwG19WpdKmbbM-Lr8958-48mYpCV0B1IAXnoNGQTL7MI8MKf-DK1I7hHhB_5NZdwuUzE3jOPyjPkqonai5xiC5b-QOySFYgibSohtlBxNp7bDaCq8tqt-8dpvfaVuB-rdW73L9pfrwwwpOEW7P7sIbmT9EPoivPFVgKVAWHXLPA2INk5o7wSKvpdazYCxpVZf7sBDVDhZOk5I9mwI1S6tfkBj0wVZz-gzaJuOYChjCqlFfT4ZJA2tPaH25jN-YpnlabcJYE_w-3T__9IeFixVc4GoIAjKUGYK2USULREiPNRu7d_HI4Uy-Odnpg8pdHyFzBWSbnSrBFdkYDGNemKa6Df9SN3Q4tEli9_aEkOZL7Xj15amz2W4nrGl8J74TgNwYxm-Uhtn73zYeuMMx_YulxX5m7kuup64uip4Kec';
-        // $token = UserData::where('user_id', Auth::user()->id)->get()[0];
+        $token = UserData::where('user_id', Auth::user()->id)->get()[0];
         $headers = [ 
             "Accept: application/vnd.allegro.public.v1+json", 
-            "Authorization: Basic " . base64_encode("$clientId:$clientSecret")
+            "Authorization: Basic " . base64_encode("$this->clientId:$$this->clientSecret")
         ];
 
         $post = [
@@ -231,19 +231,19 @@ class AllegroController extends Controller
         $users = array();
 
         // --- PRODUKCJA --- 
-        $tokens = UserData::where('user_id', 7)->get();
-        foreach ($tokens as $token)
+        $userDatas = UserData::where('user_id', 7)->get();
+        foreach ($userDatas as $userData)
         {
             // dd($token);
             $response = Http::withHeaders([
                 "Accept" => "application/vnd.allegro.public.v1+json",
-                "Authorization" => "Bearer $token->access_token"
+                "Authorization" => "Bearer $userData->access_token"
             ])->get("https://api.allegro.pl/me"); 
             if(!isset($response["error"])) {   
                 $user[] = json_decode($response);
             }
             else {
-                redirect("/get_auth?refresh_token=$token->refresh_token");
+                $this->refreshTokenRepo($userData->refresh_token);
                 $user[] = json_decode($response);
             }  
             $user[] = json_decode($response);
