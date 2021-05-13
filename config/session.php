d184c0303f684df003851478860ec742c19e530e
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-05-13 12:14:58 +0200

    CORS - err 302

--- config/session.php
+++ config/session.php
@@ -171,7 +171,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    // 'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
