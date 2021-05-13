8c1dc788674ae1f5d23e23071b5ad59d82f736a4
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-05-13 10:45:26 +0200

    CORS - problem

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,12 +9,8 @@ use AsocialMedia\AllegroApi\AllegroApi;
 
 class MainController extends Controller
 {
-    public static function csrfToken(Request $request)
-    {   
-        $token = $request->session()->token();
-
-        $token = csrf_token();
-
-        return $token;
-    }
+    // public function NAME()
+    // {
+    //     return "xD";
+    // } 
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,8 +13,5 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/register',
-        '/login',
-        'https://kodomat.herokuapp.com/',
-        'https://kodomat.herokuapp.com/*',
     ];
 }
