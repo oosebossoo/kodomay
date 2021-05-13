482e885482c062031aeb413ad08ca93cc16bab4b
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-05-13 10:22:12 +0200

    CORS - problem

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/register',
+        'login',
         'https://kodomat.herokuapp.com/',
         'https://kodomat.herokuapp.com/*',
     ];
