cef5e37b03d73c9766d8fb61bc78679c8252da3f
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-06-10 13:40:52 +0200

    0.4.16

--- app/Http/Controllers/AllegroController.php
+++ app/Http/Controllers/AllegroController.php
@@ -721,7 +721,7 @@ class AllegroController extends Controller
         foreach($orders as $order)
         {
             $customer = Customer::where('customer_id', $order->customer_id)->first();
-            $res[] = $customer;
+            $res[$order->order_id] = [ $order->order_id, $customer ];
             // echo "1 ".$customer->customer_id."<br>";
             // echo "--".$customer->first_name."<br>"; 
             // if($customer->first_name != "")
