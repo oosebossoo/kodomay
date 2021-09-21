7f904d9b50813e8ed9d9fb3f7bbbe6e211ae0cad
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-09-21 13:41:24 +0200

    cors - dziala

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -18,7 +18,7 @@ class StatisticsController extends Controller
 
     public function ordersTodayCount(Request $request)
     {
-        $user_id = $this->user;
+        $user_id = $this->user->id;
 
         return Orders::where('seller_id', $user_id)->whereBetween('order_date', [date('Y-m-d')."T00:00:00.000Z", date('Y-m-d')."T23:59:59.999Z"])->count();
     }
