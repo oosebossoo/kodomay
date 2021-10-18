bba2e6c365ef395786d751d911c197365acc180c
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-10-18 17:07:27 +0200

    main_function /api/allegro/main_function,  sprawadzanie i naprawianie

--- app/Repositories/AllegroAccountRepository.php
+++ app/Repositories/AllegroAccountRepository.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\MailController;
 
 // use App\Models\Customer;
 use App\Models\UserData;
-// use App\Models\Orders;
+use App\Models\Orders;
 // use App\Models\OrdersTable;
 use App\Models\Offers;
 // use App\Models\SentMail;
