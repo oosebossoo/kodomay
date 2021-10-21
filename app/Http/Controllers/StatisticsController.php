56ed5bc107371fff7b94e2ee705e53e7acd1b6ff
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-10-21 13:20:00 +0200

    api/allegro/... - sprawadzanie i naprawianie

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -123,6 +123,4 @@ class StatisticsController extends Controller
     function days_in_month($month, $year){
         return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
     } 
-
-
 }
