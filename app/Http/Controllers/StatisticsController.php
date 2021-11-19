abad82b639a1222ea42104baebb4e9639b7e7e03
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-11-19 14:51:38 +0100

    template_email

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -175,7 +175,7 @@ class StatisticsController extends Controller
 
     public function incomeThisMounth()
     {
-        return 0;
+        return Orders::where('seller_id', $this->user->id)->whereBetween('order_date', [date('Y-m')."-01T00:00:00.000Z", date('Y-m')."-31T23:59:59.999Z"])->sum('order_price');
     }
 
     public function sendCodesToday()
