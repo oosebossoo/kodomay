db14fdd8977e019024132d492f5234a072e271d4
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2021-12-09 11:57:06 +0100

    test-admin

--- routes/api.php
+++ routes/api.php
@@ -144,6 +144,6 @@ Route::group([
 ], function ($router) {
     // płatności
     Route::get('/test', [AdminController::class, 'test']);
-    Route::get('/dash', [PaymentController::class, 'dash']);
+    Route::get('/dash', [AdminController::class, 'dash']);
     // Route::get('/history', [PaymentController::class, 'history']);
 });
