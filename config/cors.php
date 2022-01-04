cc2f145a7a59c4abe2abfe0aede7694aa0ee8c9f
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2022-01-04 18:40:59 +0100

    test cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['/api*', '/stat*'],
+    'paths' => ['/api*', '/stat/orders/today/count'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'allowed_origins' => ['https://kodomat-front-end.herokuapp.com'],
     'allowed_origins_patterns' => [],
