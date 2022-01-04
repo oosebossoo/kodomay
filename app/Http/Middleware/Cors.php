64723e66ae6eecdeed89a7df588820a09f75a830
Sebastian Kasprzak <sebek.kasprzak@gmail.com>
2022-01-04 16:14:15 +0100

    test cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://kodomat-front-end.herokuapp.com')
+            ->header('Access-Control-Allow-Origin', 'https://kodomat-front-end.herokuapp.com/')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent')
             ->header('Access-Control-Allow-Credentials',' true');
